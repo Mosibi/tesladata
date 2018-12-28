b728e2ada3fb0919df6f2bee2d9cd50099420de6
Richard Arends <github@mosibi.nl>
2018-12-28 15:58:34 +0100

    code reformat

--- src/poller.py
+++ src/poller.py
@@ -14,43 +14,73 @@ import teslajson
 
 def debug_msg(message):
     if DEBUG is True:
-        print('{0} DEBUG: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
+        print(
+            "{0} DEBUG: {1}".format(
+                time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message
+            )
+        )
+
 
 def error_msg(message):
-    print('{0} ERROR: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
+    print(
+        "{0} ERROR: {1}".format(
+            time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message
+        )
+    )
+
 
 def warning_msg(message):
-    print('{0} WARNING: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
+    print(
+        "{0} WARNING: {1}".format(
+            time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message
+        )
+    )
+
 
 def info_msg(message):
-    print('{0} INFO: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
+    print(
+        "{0} INFO: {1}".format(
+            time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message
+        )
+    )
+
 
 def read_config(configfile):
     try:
-        with open(configfile, 'r') as ymlfile:
+        with open(configfile, "r") as ymlfile:
             cfg = yaml.load(ymlfile)
     except Exception as err:
-        error_msg('could not open/read config file {}: {}'.format(configfile, err))
+        error_msg("could not open/read config file {}: {}".format(configfile, err))
 
     return cfg
 
+
 def get_all_cardata(vehicle):
-    endpoints = {'charge_state', 'climate_state', 'drive_state', 'gui_settings', 'vehicle_state', 'vehicle_config'}
+    endpoints = {
+        "charge_state",
+        "climate_state",
+        "drive_state",
+        "gui_settings",
+        "vehicle_state",
+        "vehicle_config",
+    }
     data = {}
 
     for endpoint in endpoints:
         try:
             endpoint_data = vehicle.data_request(endpoint)
-            endpoint_data['vin'] = vehicle['vin']
+            endpoint_data["vin"] = vehicle["vin"]
 
-            data.update({
-                endpoint: endpoint_data
-            })
+            data.update({endpoint: endpoint_data})
 
-            debug_msg('get_all_cardata: {}, data: {}'.format(endpoint, data))
+            debug_msg("get_all_cardata: {}, data: {}".format(endpoint, data))
 
         except Exception as err:
-            warning_msg('Could not get data from Tesla with vin {}: {}'.format(vehicle['vin'], str(err)))
+            warning_msg(
+                "Could not get data from Tesla with vin {}: {}".format(
+                    vehicle["vin"], str(err)
+                )
+            )
 
     return data
 
@@ -62,22 +92,24 @@ def mongo_write(db, collection_name, json_data):
 
         try:
             result = collection.insert_one(json_data)
-            debug_msg('Inserted a document into mongo: {}'.format(result.inserted_id))
+            debug_msg("Inserted a document into mongo: {}".format(result.inserted_id))
         except Exception as err:
-            error_msg('An error occured while writing data to Mongo, {}'.format(err))
-            error_msg('The following was not written to mongo: {}'.format(json_data))
+            error_msg("An error occured while writing data to Mongo, {}".format(err))
+            error_msg("The following was not written to mongo: {}".format(json_data))
             sys.exit(1)
 
 
 def iterate_data(data):
     for i in data:
-        print('{}: {}'.format(i, data[i]))
+        print("{}: {}".format(i, data[i]))
+
 
 def sleepy(drive_state):
-    if drive_state['shift_state'] is None:
+    if drive_state["shift_state"] is None:
         return True
     else:
         return False
+
 
 def connect_to_tesla_api(username, password):
     output = None
@@ -85,9 +117,10 @@ def connect_to_tesla_api(username, password):
     try:
         output = teslajson.Connection(username, password)
     except Exception as err:
-        warning_msg('Could not connect to the Tesla API: {}'.format(str(err)))
+        warning_msg("Could not connect to the Tesla API: {}".format(str(err)))
 
     return output
+
 
 def get_vehicle(username, password, vin):
     vehicle = None
@@ -95,7 +128,7 @@ def get_vehicle(username, password, vin):
 
     try:
         for v in connection.vehicles:
-            if v['vin'] == vin:
+            if v["vin"] == vin:
                 vehicle = v
     except AttributeError:
         # If getting data failed, a AttributeError is raised when
@@ -104,13 +137,14 @@ def get_vehicle(username, password, vin):
         pass
 
     if vehicle is None:
-        error_msg('Could not get/find vehicle information from the Tesla API')
+        error_msg("Could not get/find vehicle information from the Tesla API")
         sys.exit(99)
     else:
         if DEBUG is True:
-            print('vehicle: {}'.format(dict(vehicle)))
+            print("vehicle: {}".format(dict(vehicle)))
 
         return vehicle
+
 
 def poller(vehicle, last_time_sleepy, mongo_db):
     if last_time_sleepy > 0:
@@ -118,39 +152,58 @@ def poller(vehicle, last_time_sleepy, mongo_db):
         # But if the car is allready sleepy for 30 minutes, set
         # last_time_sleepy to 0 and start polling it again.
 
-        seconds_sleepy = (int(time.time()) - last_time_sleepy)
+        seconds_sleepy = int(time.time()) - last_time_sleepy
 
         if seconds_sleepy < 3600:
             minutes = int(seconds_sleepy / 60)
-            info_msg('Tesla with vin {} is sleepy, but still online for {} minutes'.format(vehicle['vin'], minutes))
+            info_msg(
+                "Tesla with vin {} is sleepy, but still online for {} minutes".format(
+                    vehicle["vin"], minutes
+                )
+            )
             return last_time_sleepy
         else:
-            info_msg('Tesla with vin {} is 60 minutes sleepy, we start polling it again'.format(vehicle['vin']))
+            info_msg(
+                "Tesla with vin {} is 60 minutes sleepy, we start polling it again".format(
+                    vehicle["vin"]
+                )
+            )
             last_time_sleepy = 0
     else:
         try:
             car_data = get_all_cardata(vehicle)
-            charge_state = car_data['charge_state']
-     
+            charge_state = car_data["charge_state"]
+
             # Write data for every endpoint polled from the Tesla to Mongo
             for endpoint in car_data:
                 mongo_write(mongo_db, endpoint, car_data[endpoint])
-            
+
             """
             Detect if the car want's to go to sleep. If the car is charging we keep polling it,
             preventing it to go to sleep.
             """
-            if sleepy(car_data['drive_state']) is True and charge_state['charging_state'] != 'Charging':
-                info_msg('Tesla with vin {} is sleepy'.format(vehicle['vin']))
-                data = {"timestamp": int(round(time.time() * 1000)), "sleepy": True, "vin": vehicle['vin']}
-                mongo_write(mongo_db, 'custom_data', data)
+            if (
+                sleepy(car_data["drive_state"]) is True
+                and charge_state["charging_state"] != "Charging"
+            ):
+                info_msg("Tesla with vin {} is sleepy".format(vehicle["vin"]))
+                data = {
+                    "timestamp": int(round(time.time() * 1000)),
+                    "sleepy": True,
+                    "vin": vehicle["vin"],
+                }
+                mongo_write(mongo_db, "custom_data", data)
                 last_time_sleepy = int(time.time())
 
                 # wait 20 minutes before doing anything else
                 time.sleep(1200)
         except KeyError as endpoint_name:
-            error_msg('No data received from Tesla with vin {} for endpoint {}'.format(vehicle['vin'], endpoint_name))
-     
+            error_msg(
+                "No data received from Tesla with vin {} for endpoint {}".format(
+                    vehicle["vin"], endpoint_name
+                )
+            )
+
     return last_time_sleepy
 
 
@@ -162,46 +215,55 @@ def main():
 
     # Get the config file
     configfile = None
-    for filename in ['/usr/local/tesladata/config.yaml', './src/config.yaml', './config.yaml']:
+    for filename in [
+        "/usr/local/tesladata/config.yaml",
+        "./src/config.yaml",
+        "./config.yaml",
+    ]:
         if os.path.isfile(filename):
             configfile = filename
 
     if configfile is None:
-        error_msg('Could not find a configuration file for Tesladata')
+        error_msg("Could not find a configuration file for Tesladata")
         sys.exit(99)
 
     config = read_config(configfile)
-    DEBUG = config['debug']
-    WRITE_TO_MONGO = config['write_to_mongo']
+    DEBUG = config["debug"]
+    WRITE_TO_MONGO = config["write_to_mongo"]
 
     if WRITE_TO_MONGO is True:
-        client = MongoClient('mongodb://{}:27017/'.format(config['mongo_server']))
-        mongo_db = client[config['mongo_database']]
+        client = MongoClient("mongodb://{}:27017/".format(config["mongo_server"]))
+        mongo_db = client[config["mongo_database"]]
 
     while True:
         # On every loop, fetch fresh vehicle information from the Tesla API.
-        vehicle = get_vehicle(config['username'], config['password'], config['vin'])
-        vehicle['timestamp'] = int(round(time.time() * 1000))
+        vehicle = get_vehicle(config["username"], config["password"], config["vin"])
+        vehicle["timestamp"] = int(round(time.time() * 1000))
 
         # Write 'vehicle' information in Mongo
-        mongo_write(mongo_db, 'vehicle', dict(vehicle))
+        mongo_write(mongo_db, "vehicle", dict(vehicle))
 
-        if vehicle['state'] == 'online':
+        if vehicle["state"] == "online":
             last_time_sleepy = poller(vehicle, last_time_sleepy, mongo_db)
-        elif vehicle['state'] == 'offline':
-            info_msg('Tesla with vin {} is offline'.format(vehicle['vin']))
+        elif vehicle["state"] == "offline":
+            info_msg("Tesla with vin {} is offline".format(vehicle["vin"]))
             last_time_sleepy = 0
-        elif vehicle['state'] == 'asleep':
-            info_msg('Tesla with vin {} is sleeping'.format(vehicle['vin']))
+        elif vehicle["state"] == "asleep":
+            info_msg("Tesla with vin {} is sleeping".format(vehicle["vin"]))
             last_time_sleepy = 0
-        elif vehicle['state'] == 'waking':
-            info_msg('Tesla with vin {} is waking up'.format(vehicle['vin']))
+        elif vehicle["state"] == "waking":
+            info_msg("Tesla with vin {} is waking up".format(vehicle["vin"]))
             last_time_sleepy = 0
         else:
-            warning_msg('Tesla with vin {} has state {}. This state is unknown to us'.format(vehicle['vin'], vehicle['state']))
+            warning_msg(
+                "Tesla with vin {} has state {}. This state is unknown to us".format(
+                    vehicle["vin"], vehicle["state"]
+                )
+            )
             last_time_sleepy = 0
 
-        time.sleep(config['poll_interval'])
+        time.sleep(config["poll_interval"])
+
 
 if __name__ == "__main__":
     sys.exit(main())
