edca7ea16138aa71b820ef6f9bd45d8e76350ca7
Richard Arends <github@mosibi.nl>
2019-01-05 13:37:22 +0100

    Moved the sleepy_to_sleep function to tesladata so that both sleepy-to-sleeping.py and mongo-to-influxdb.py can use it

--- src/mongo-to-influxdb.py
+++ src/mongo-to-influxdb.py
@@ -11,45 +11,6 @@ import tesladata
 from tesladata import log
 
 
-def sleepy_to_sleep(mongo_db, influx_server="localhost"):
-    """
-    sleepy_to_sleep calculates the difference in seconds between
-    the state 'sleepy' and 'asleep'
-
-    params:
-      mongo_db: a mongodb database handle
-      influx_server: the hostname of the influxdb server
-    """
-    custom_data = tesladata.get_mongo_data(
-        mongo_db,
-        "custom_data",
-        timestamp_start=tesladata.generate_timestamp(min_secs=86400),
-    )
-
-    for doc in custom_data:
-        ts_start = doc["timestamp"]
-        ts_end = tesladata.generate_timestamp(timestamp=ts_start, add_secs=3600)
-
-        vehicle_data = tesladata.get_mongo_data(
-            mongo_db, "vehicle", timestamp_start=ts_start, timestamp_end=ts_end
-        )
-
-        for vdata in vehicle_data:
-            if vdata["state"] == "asleep":
-                ts_asleep = vdata["timestamp"]
-
-                tesladata.influx_write(
-                    servername=influx_server,
-                    measurement="custom_data",
-                    entity="sleepy_to_sleep",
-                    vin=vdata["vin"],
-                    value=(ts_asleep - ts_start) / 1000,
-                    ms=vdata["timestamp"],
-                )
-
-                break
-
-
 def debug(msg):
     if DEBUG is True:
         log(msg, level="DEBUG")
@@ -87,7 +48,11 @@ def main():
     client = tesladata.mongoclient(config["mongo_server"])
     mongo_db = client[config["mongo_database"]]
 
-    sleepy_to_sleep(mongo_db=mongo_db, influx_server=config["influx_server"])
+    tesladata.sleepy_to_sleep(
+        mongo_db=mongo_db,
+        influx_server=config["influx_server"],
+        secondsback=secondsback,
+    )
 
     for collection in ["charge_state", "vehicle", "custom_data", "vehicle_state"]:
         debug("Working on collection {}".format(collection))
